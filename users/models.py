f9635a959ff58faf907d649e4289b19754545e9e
racheal-sure <mnyaboe33@gmail.com>
2024-11-25 23:02:32 +0300

    More updates

--- users/models.py
+++ users/models.py
@@ -1,12 +1,29 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_patient = models.BooleanField(default=False)
     is_doctor = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
 
+    # Override default fields with custom related_name
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_groups",
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
+
     def __str__(self):
         return self.username
+
 
 
